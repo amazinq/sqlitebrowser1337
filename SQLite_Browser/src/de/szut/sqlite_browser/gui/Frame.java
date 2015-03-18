65b9b802f17d55ec28b56f36caf3d37c79d97980
amazinq <steffen.wissmann@hotmail.com>
2015-03-18 12:34:29 +0100

    Finished basic GUI design

--- SQLite_Browser/src/de/szut/sqlite_browser/gui/Frame.java
+++ SQLite_Browser/src/de/szut/sqlite_browser/gui/Frame.java
@@ -1,6 +1,8 @@
 package de.szut.sqlite_browser.gui;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.GridLayout;
 
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
@@ -8,30 +10,25 @@ import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.JTree;
 import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableModel;
 
 public class Frame extends JFrame {
-
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 5701910777941753073L;
-	private JTextField queryField;
-	private JTable contentTable;
-	private JTextField limitFrom;
-	private JTextField limitTo;
-	private JCheckBox limitCheckBox;
+	private static final long serialVersionUID = 5836166086363876139L;
+	private JTextField commandTextField;
+	private JTable dataTable;
+	private JTextField limitLowerBoundTextField;
+	private JTextField limitUpperBoundTextField;
 	private JTree dataBaseTree;
-	private JScrollPane scrollPaneContentTable;
-	private JScrollPane scrollPaneDataBaseTree;
-	private JLabel connectionLabel;
-	private JMenuBar menuBar;
-	private JMenu itemMenu;
-	private JMenuItem openMenuItem;
 
 	/**
 	 * Launch the application.
@@ -54,57 +51,66 @@ public class Frame extends JFrame {
 	 */
 	public Frame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 800, 620);
-		setResizable(false);
-
-		menuBar = new JMenuBar();
+		setBounds(100, 100, 868, 582);
+		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
-
-		itemMenu = new JMenu("File");
-		menuBar.add(itemMenu);
-
-		openMenuItem = new JMenuItem("Open");
-		itemMenu.add(openMenuItem);
-		getContentPane().setLayout(null);
-
-		scrollPaneDataBaseTree = new JScrollPane();
-		scrollPaneDataBaseTree.setBounds(6, 11, 142, 530);
-		getContentPane().add(scrollPaneDataBaseTree);
-
+		JMenu fileMenu = new JMenu("File");
+		menuBar.add(fileMenu);
+		JMenuItem openDataBaseMenuItem = new JMenuItem("Open Database");
+		fileMenu.add(openDataBaseMenuItem);
+		JPanel panel = new JPanel();
+		setContentPane(panel);
+		panel.setLayout(new BorderLayout(0, 0));
+		JPanel statePanel = new JPanel();
+		panel.add(statePanel, BorderLayout.SOUTH);
+		statePanel.setLayout(new BorderLayout(0, 0));
+		JLabel stateLabel = new JLabel("MySQL Connected ");
+		stateLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		statePanel.add(stateLabel, BorderLayout.NORTH);
+		JSplitPane splitPane = new JSplitPane();
+		panel.add(splitPane, BorderLayout.CENTER);
+		JPanel dataPanel = new JPanel();
+		splitPane.setRightComponent(dataPanel);
+		dataPanel.setLayout(new BorderLayout(0, 0));
+		JPanel limitPanel = new JPanel();
+		dataPanel.add(limitPanel, BorderLayout.SOUTH);
+		limitPanel.setLayout(new GridLayout(0, 3, 0, 0));
+		
+		JCheckBox limitCheckBox = new JCheckBox("Limit: ");
+		limitPanel.add(limitCheckBox);
+		
+		limitLowerBoundTextField = new JTextField();
+		limitPanel.add(limitLowerBoundTextField);
+		limitLowerBoundTextField.setToolTipText("Set Lower Bounds");
+		limitLowerBoundTextField.setColumns(10);
+		
+		limitUpperBoundTextField = new JTextField();
+		limitPanel.add(limitUpperBoundTextField);
+		limitUpperBoundTextField.setToolTipText("Set Upper Bounds");
+		limitUpperBoundTextField.setColumns(10);
+		JPanel commandPanel = new JPanel();
+		dataPanel.add(commandPanel, BorderLayout.NORTH);
+		commandPanel.setLayout(new BorderLayout(0, 0));
+		commandTextField = new JTextField();
+		commandPanel.add(commandTextField);
+		commandTextField.setColumns(10);
+		JScrollPane tableScrollPane = new JScrollPane();
+		dataPanel.add(tableScrollPane, BorderLayout.CENTER);
+		dataTable = new JTable();
+		dataTable.setModel(new DefaultTableModel(new Object[][] {
+				{ null, null, null, null, null, null },
+				{ null, null, null, null, null, null },
+				{ null, null, null, null, null, null },
+				{ null, null, null, null, null, null },
+				{ null, null, null, null, null, null },
+				{ null, null, null, null, null, null },
+				{ null, null, null, null, null, null }, }, new String[] {
+				"New column", "New column", "New column", "New column",
+				"New column", "New column" }));
+		tableScrollPane.setViewportView(dataTable);
+		JScrollPane treeScrollPane = new JScrollPane();
+		splitPane.setLeftComponent(treeScrollPane);
 		dataBaseTree = new JTree();
-		scrollPaneDataBaseTree.setViewportView(dataBaseTree);
-
-		queryField = new JTextField();
-		queryField.setBounds(158, 11, 626, 20);
-		getContentPane().add(queryField);
-		queryField.setColumns(10);
-
-		scrollPaneContentTable = new JScrollPane();
-		scrollPaneContentTable.setBounds(158, 37, 626, 504);
-		getContentPane().add(scrollPaneContentTable);
-
-		contentTable = new JTable();
-		contentTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-		scrollPaneContentTable.setViewportView(contentTable);
-
-		connectionLabel = new JLabel("New label");
-		connectionLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-		connectionLabel.setBounds(660, 541, 124, 27);
-		getContentPane().add(connectionLabel);
-
-		limitCheckBox = new JCheckBox("Limit");
-		limitCheckBox.setBounds(6, 545, 97, 23);
-		getContentPane().add(limitCheckBox);
-
-		limitFrom = new JTextField();
-		limitFrom.setBounds(158, 546, 100, 20);
-		limitFrom.setText("von");
-		getContentPane().add(limitFrom);
-
-		limitTo = new JTextField();
-		limitTo.setBounds(268, 546, 100, 20);
-		limitTo.setText("bis");
-		getContentPane().add(limitTo);
-
+		treeScrollPane.setViewportView(dataBaseTree);
 	}
 }
