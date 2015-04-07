bba4bb0cc118746b6a13adb56d9f572768a5c6cc
Steffen <Steffen@Steffen-PC.multi.box>
2015-04-07 16:41:16 +0200

    Added surfaceinterface

---
+++ SQLite_Browser/src/de/szut/sqlite_browser/gui/Surface.java
@@ -0,0 +1,9 @@
+package de.szut.sqlite_browser.gui;
+
+public interface Surface {
+
+	public void updateTree();
+	
+	public void updateDataList();
+	
+}
---
+++ SQLite_Browser/src/de/szut/sqlite_browser/gui/SwingSurface.java
@@ -0,0 +1,24 @@
+package de.szut.sqlite_browser.gui;
+
+import javax.swing.JPanel;
+
+public class SwingSurface extends JPanel implements Surface {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1327566355315618207L;
+
+	@Override
+	public void updateTree() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateDataList() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
